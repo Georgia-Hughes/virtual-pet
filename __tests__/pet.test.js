26bf6977ebc17be9e9e9ccf2bc0ed6cddb02e2f4
Georgia <georgia@webantic.co.uk>
2018-09-10 15:24:01 +0100

    update my pet - stage 5

---
+++ __tests__/pet.test.js
@@ -0,0 +1,34 @@
+const Unicorn = require('../src/pet');
+
+describe('constructor', () => {
+ it('returns an object', () => {
+  const pet = new Unicorn('Jerry');
+  expect(pet.name).toEqual('Jerry');
+ });
+});
+
+describe('constructor', () => {
+ it('has initial age of 0', () => {
+  const pet = new Unicorn('Jerry');
+  expect(pet.age).toEqual(0);
+ });
+});
+
+describe('growUp', () => {
+ it('increase the age by 1', () => {
+  const pet = new Unicorn('Jerry');
+  pet.growUp();
+  expect(pet.age).toEqual(1);
+ });
+ it('increase the hunger by 5', () => {
+  const pet = new Unicorn('Jerry');
+  pet.feed();
+  expect(pet.hunger).toEqual(5);
+ });
+ it('decreases fitness by 3', () => {
+  const pet = new Unicorn('Jerry');
+  pet.growUp();
+  expect(pet.fitness).toEqual(7);
+ });
+});
+
---
+++ src/pet.js
@@ -0,0 +1,23 @@
+function Unicorn(name, age) {
+ this.name = name;
+ this.age = 0;
+ this.hunger = 0;
+ this.fitness = 10;
+
+ Unicorn.prototype.growUp = function() {
+  this.age += 1;
+  this.hunger += 5;
+  this.fitness -= 3;
+ };
+
+ Unicorn.prototype.feed = function() {
+  this.hunger += 5;
+ }
+
+ const pet = {
+  name: 'Jerry'
+ };
+
+}
+
+module.exports = Unicorn;
